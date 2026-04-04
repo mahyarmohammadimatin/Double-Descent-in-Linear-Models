2315eb0a632ae91af212c562518188ca49a7d338
mahyarmohammadimatin <mahyarmohammadimatin@gmail.com>
2026-04-04 21:40:29 +0330

    transfer functions into classes DDSimulation & SyntheticData

--- functions.py
+++ functions.py
@@ -1,46 +1,78 @@
+from utils import *
 import numpy as np
+from itertools import product
+import matplotlib.pyplot as plt
 
-def synthetic_linear_reg_data_generate(n, d, n_train, seed, noise_std=0.1):
-    """
-        X : Feature matrix (n, d)
-        y : Noisy targets (n,)
-        rw : real weights (d,)
-    """
-    rng = np.random.default_rng(seed)
-    X = rng.normal(loc=0.0, scale=1.0, size=(n, d))
+class SyntheticData:
+    def __init__(self, n, dim, seed=1):
+        self.rng = np.random.default_rng(seed)
+        self.n = n
+        self.dim = dim
+        self.data = self.rng.normal(loc=0.0, scale=1.0, size=(n, dim))
+    def generate_linear_regression_data(self, noise_std=0.1):
+        """
+            X (data) : Feature matrix (n, d)
+            y : Noisy targets (n,)
+            rw : real weights (d,)
+        """
+        X = self.data
+        # Randomly generate real weights: Scaling by 1/sqrt(d) keeps signal magnitude more stable as d changes
+        rw = self.rng.normal(loc=0.0, scale=1.0 / np.sqrt(self.dim), size=self.dim)
+        noise = self.rng.normal(loc=0.0, scale=noise_std, size=self.n)
+        y = X @ rw + noise
+        return X, y
+    @staticmethod
+    def split_train_test(X, y, n_train):
+        X_train = X[:n_train]
+        y_train = y[:n_train]
+        X_test = X[n_train:]
+        y_test = y[n_train:]
+        return X_train, y_train, X_test, y_test
 
-    # Randomly generate real weights: Scaling by 1/sqrt(d) keeps signal magnitude more stable as d changes
-    rw = rng.normal(loc=0.0, scale=1.0 / np.sqrt(d), size=d)
-    
-    noise = rng.normal(loc=0.0, scale=noise_std, size=n)
-    y = X @ rw + noise
-    
-    X_train = X[:n_train]
-    y_train = y[:n_train]
-    X_test = X[n_train:]
-    y_test = y[n_train:]
-    
-    return X_train, y_train, X_test, y_test, rw
+class DDSimulation:
+    def __init__(self, model, n_train, n_test, dim_values, seed_values, noise_values, lam_values):
+        # Fixed Attrs
+        self.model = model
+        self.n_train = n_train
+        self.n_test = n_test
 
-def fit_least_squares(X, y): # TODO: change to self implemented function
-    return np.linalg.pinv(X) @ y
-def predict(X, w):
-    return X @ w
-def mse(y_true, y_pred):
-    return np.mean((y_true - y_pred) ** 2)
+        # Variable Attrs
+        self.dim_values = dim_values
+        self.seed_values = seed_values
+        self.noise_values = noise_values
+        self.lam_values = lam_values
 
-def fit_model_to_synthetic_data(model, seed_values, **data_gen_kwargs):
-    mse_train = []
-    mse_test = []
-    for seed in seed_values:
-        X_train,y_train,X_test,y_test,rw = synthetic_linear_reg_data_generate(seed=seed, **data_gen_kwargs)
-        
-        if model=='ls':
+    def fit_model_to_specific_config(self, seed, dim, noise_std, lam=None):
+        synthetic_data = SyntheticData(n=self.n_train+self.n_test, dim=dim, seed=seed)
+        X, y = synthetic_data.generate_linear_regression_data(noise_std=noise_std)
+        X_train, y_train, X_test, y_test = synthetic_data.split_train_test(X, y, n_train=self.n_train)
+
+        if self.model == 'ls':
             w_hat = fit_least_squares(X_train, y_train)
+        elif self.model == 'ridge':
+            w_hat = fit_ridge_regression(X_train, y_train, lam)
+        else:
+            raise ValueError("model must be 'ls' or 'ridge'")
 
         y_train_pred = predict(X_train, w_hat)
         y_test_pred = predict(X_test, w_hat)
-        
-        mse_train.append(mse(y_train, y_train_pred))
-        mse_test.append(mse(y_test, y_test_pred))
-    return np.mean(mse_train), np.mean(mse_test)
+
+        return mse(y_train, y_train_pred), mse(y_test, y_test_pred)
+
+    def run_simulation(self):
+        results = {}
+        for noise_std, lam in product(self.noise_values, self.lam_values):
+            train_errors, test_errors = [], []
+            for dim in self.dim_values:
+                mse_trains, mse_tests = [], []
+                for seed in self.seed_values:
+                    mse_train, mse_test = self.fit_model_to_specific_config(seed=seed, dim=dim, noise_std=noise_std, lam=lam)
+                    mse_trains.append(mse_train)
+                    mse_tests.append(mse_test)
+                # Average on different seeds
+                train_errors.append(np.mean(mse_trains))
+                test_errors.append(np.mean(mse_tests))
+            # for each combination of (noise_std, lam) save list of errors corresponding to dimension values
+            results[f'train_{noise_std}_{lam}'] = train_errors
+            results[f'test_{noise_std}_{lam}'] = test_errors
+
